ed3230385ea3839920cff39aebd3bcf775f124ba
sebastian <kras0194@gmail.com>
2016-06-15 13:44:14 +0200

    Implementacja kodu

--- Kolokwium_Kranczuk_Sebastian/src/Triplet.java
+++ Kolokwium_Kranczuk_Sebastian/src/Triplet.java
@@ -1,3 +1,9 @@
+
+import java.lang.reflect.Array;
+
+
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,4 +16,26 @@
  */
 public class Triplet {
     
+    public int maxValue()
+    {
+        int max = 0;
+        int value = 0;
+        for (int i = 0; i < 6; ++i)
+        {
+            for (int j = 1; j < 6; ++j)
+            {
+                for (int k = 2; k < 6; ++k)
+                {
+                    value = tab[i] * tab[j] * tab[k];
+                    if (value > max)
+                    {
+                        max = value;
+                    }
+                }
+            }
+        }
+        return max;
+    }
+    
+    public int[] tab = {-3, 1, 2, -2, 5, 6};
 }
